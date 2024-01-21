41c6df8ec5845c457697ad4e0e6b2148af91984b
Docartes <ahmadv0101@gmail.com>
2024-01-21 11:40:42 +0700

    FreeCodeCamp - Header Parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+	const ipAddress = req.socket.remoteAddress;
+	const lang = req.headers["accept-language"];
+	const user_agent = req.headers["user-agent"];
+
+  res.json({ 
+  	ip: ipAddress, 
+  	lang: lang,
+  	software: user_agent
+  });
 });
 
 // listen for requests :)
